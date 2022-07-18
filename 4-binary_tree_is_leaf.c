b97611d4e0c118815b0dbf8dca863879401aaf74
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:28:32 -0300

    delete tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_is_leaf - check if node is leaf
+ * @node: node
+ * Return: 1 if is leaf or 0 if fail or not ies leaf
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
