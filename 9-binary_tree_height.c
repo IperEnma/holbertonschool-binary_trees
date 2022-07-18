4eafa506e929c192b9f87ea6d2e91b0662dbcc9e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 10:51:45 -0300

    max height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,16 +4,29 @@
  * @tree: tree
  * Return: height
  */
+size_t max_height(size_t left, size_t right)
+{
+	if (left == right)
+		return (left);
+	if (left > right)
+		return (left);
+	if (right > left)
+		return (right);
+	return (0);
+}
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
 	if (tree->left)
-		height = (1 + binary_tree_height(tree->left));
+		left_height = (1 + binary_tree_height(tree->left));
 	if (tree->right)
-		height = (1 + binary_tree_height(tree->right));
-	return (height);
+		right_height = (1 + binary_tree_height(tree->right));
+
+	return (max_height(left_height, right_height));
 }
--- 9-main.c
+++ 9-main.c
@@ -14,11 +14,13 @@ int main(void)
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
+	binary_tree_insert_left(root->left, 6);
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
+    root->right = binary_tree_insert_right(root, 128);
+	binary_tree_insert_left(root->right, 128);
+	/*binary_tree_insert_left(root->left, 222);*/
     binary_tree_print(root);
-
     height = binary_tree_height(root);
     printf("Height from %d: %lu\n", root->n, height);
     height = binary_tree_height(root->right);
