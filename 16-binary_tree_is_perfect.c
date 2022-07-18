499a4fff567d94a079b4c3c4c9958f23a8e7e2e4
Alvaro <ajsabini@gmail.com>
2022-07-18 16:23:32 -0700

    16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -65,10 +65,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left && tree->right == NULL)
-		return (0);
-	if (tree->right && tree->left == NULL)
-		return (0);
 	if (tree->left)
 		left = binary_tree_is_full(tree->left);
 	if (tree->right)
