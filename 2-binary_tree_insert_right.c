67248c79fd55df220060d71ad20ae69db5984d47
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:24:58 -0300

    insert node right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,16 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_insert_right - insert node right
+ * @parent: parent node
+ * @value: data node
+ * Return: return new node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
+	if (parent == NULL)
+		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
@@ -16,21 +20,19 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->right = NULL;
 	new->n = value;
 
-	if (parent == NULL)
-		return (NULL);
 	if (parent->right == NULL)
-	{   
+	{
 		parent->right = new;
 		new->parent = parent;
 		return (new);
-	}   
+	}
 	else
-	{   
+	{
 		new->right = parent->right;
 		new->parent = parent;
 		parent->right = new;
 		parent->right->right->parent = new;
 		return (new);
-	}   
+	}
 }
 
