bf1088253c8f5d805178e2389ba1d09f2c8c002e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:22:36 -0300

    insert node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,10 +9,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
 
+	if (parent == NULL)
+		return (NULL);
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
-		return (NULL);
-	if (parent == NULL)
 		return (NULL);
 
 	new->parent = NULL;
