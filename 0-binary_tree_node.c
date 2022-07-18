a094d7ff3dad8e3da342ca68ca13aea201c22f60
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:07:04 -0300

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_node - function that create node
+ * @parent: node parent
+ * @value: data node
+ * Return: pointer node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = NULL;
-	
+
 	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
