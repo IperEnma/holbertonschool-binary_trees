ac0780f4be1f8613caa6ecad4b0e7f70f1fdadf2
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:29:42 -0300

    check if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ * binary_tree_is_root - check if node is root
+ * @node: node
+ * Return: 1 if is root or return 0 if fail or not is root
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
