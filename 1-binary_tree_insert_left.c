7d190c193b2cc6a383e68d2268c5a71ad5cf5a8d
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:19:47 -0300

    insert node left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_insert_left - insert node left in the arbol
+ * @parent: parent node
+ * @value: data node
+ * Return: pointer node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
