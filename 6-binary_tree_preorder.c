eebc7a690fe39f2aa1693b5375b35902c71a3ed3
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-18 08:31:17 -0300

    preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
+ * binary_tree_preorder - hrough a binary tree using pre-order traversal
+ * @func: function
+ * @tree: tree
+ * Return: void
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
