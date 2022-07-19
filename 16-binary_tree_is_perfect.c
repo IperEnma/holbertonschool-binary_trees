1f0c0595a1b7585b76c423a7345860e3955ed99d
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-19 10:58:08 -0300

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -88,7 +88,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	if (binary_tree_is_full(tree) == 0)
+	if (binary_tree_is_full(tree) == 1)
 		return (0);
 	if (binary_tree_balance(tree) != 0)
 		return (0);
