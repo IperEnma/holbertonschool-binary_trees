ba202c4df2a03dc3d90058c4e2657e09f18315a5
IperEnma <enmanuelhernandez1843@gmail.com>
2022-07-20 21:21:59 -0300

    rotate right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -13,8 +13,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	if (tree->left == NULL)
 		return (NULL);
 	root = tree->left;
-	if (root->right)
-		tree->left = root->right;
+	tree->left = root->right;
 	tree->parent = root;
 	root->right = tree;
 	root->parent = NULL;
